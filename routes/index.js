ca32fb3e1e3b1808824e9e1ecfd5fdc97d7b1982
essemfly <essemfly@gmail.com>
2017-03-20 15:41:50 +0900

    Make all file skeletons

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,55 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/fanmeeting', function(req, res, next) {
+    res.render('fanmeeting');
+});
+
+router.get('/carribean', function(req, res, next) {
+    res.render('carribean');
+});
+
+router.get('/dramaset', function(req, res, next) {
+    res.render('dramaset');
+});
+
+router.get('/hanriver', function(req, res, next) {
+    res.render('hanriver');
+});
+
+router.get('/restaurant1', function(req, res, next) {
+    res.render('restaurant1');
+});
+
+router.get('/restaurant2', function(req, res, next) {
+    res.render('restaurant2');
+});
+
+router.get('/restaurant3', function(req, res, next) {
+    res.render('restaurant3');
+});
+
+router.get('/hotel', function(req, res, next) {
+    res.render('hotel');
+});
+
+router.get('/makeup', function(req, res, next) {
+    res.render('makeup');
+});
+
+router.get('/entertainment', function(req, res, next) {
+    res.render('entertainment');
+});
+
+router.get('/nailart', function(req, res, next) {
+    res.render('nailart');
+});
+
+router.get('/skincare', function(req, res, next) {
+    res.render('skincare');
+});
+
+
 module.exports = router;
+
+
